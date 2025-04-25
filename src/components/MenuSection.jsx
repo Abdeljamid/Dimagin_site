6c5652d20ce0c1ca0290b11425cab90507476cfc
Majid <Ajouaoujamid@gmail.com>
2025-04-25 09:50:30 +0200

    modif du menu

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { X, ArrowRight } from "lucide-react";
+import { X, ArrowRight, ArrowUpRight } from "lucide-react";
 import { useDarkMode } from "../context/DarkModeContext";
 
 const MenuSection = ({
@@ -10,155 +10,149 @@ const MenuSection = ({
 }) => {
   const { darkMode } = useDarkMode();
 
+  // Liste des liens
+  const menuLinks = [
+    { id: "HeroSection" },
+    { id: "ExpertiseSection", label: "Expertise" },
+    { id: "PortfolioGrid", label: "Portfolio" },
+    { id: "Descriptif", label: "Descriptif" },
+    { id: "TeamSection", label: "Team" },
+    { id: "ValuesSection", label: "Nos Valeurs" },
+    { id: "Footer", label: "Contactez-Nous" },
+  ];
   return (
-    <section>
-      <div
-        className={`fixed top-0 left-0 w-full h-full flex z-50 font-thin ${
-          darkMode ? "bg-gray-900 text-white" : "bg-gray-100 text-black"
-        }`}
+    <div className="fixed inset-0 z-50 bg-[#232323] text-white w-screen h-screen grid grid-rows-4 grid-cols-2">
+      {/* Bloc 1 */}
+      {/* bloc1.1 */}
+      <button
+        onClick={() => {
+          scrollToSection("HeroSection");
+          toggleMenu();
+        }}
+        className="hover:bg-black transition-colors duration-200  border-r border-b border-[#2d2d2d] border-t-0 border-l-0  p-[35px] text-9xl text-start font-thin"
       >
-        {/* Partie Gauche */}
-        <div
-          className={`w-full sm:w-1/2 p-4 pt-20 md:pt-10 sm:p-8 flex flex-col justify-between border-b sm:border-r ${
-            darkMode ? "border-gray-700" : "border-gray-400"
-          } h-full`}
+        DIMAGIN
+      </button>
+      {/* bloc1.2 */}
+      <button
+        onClick={toggleMenu}
+        className={` border-r border-b border-[#2d2d2d] border-t-0 border-l-0  hover:bg-black transition-colors duration-200   p-16 flex justify-end  ${
+          darkMode
+            ? "text-gray-400 hover:text-gray-200"
+            : "text-gray-400 hover:text-gray-700"
+        }`}
+        aria-label="Fermer le menu"
+      >
+        <X size={40} />
+      </button>
+
+      {/* Bloc 2 */}
+      {/* Bloc 2.1 */}
+      <button
+        onClick={() => {
+          scrollToSection("ExpertiseSection");
+          toggleMenu();
+        }}
+        className=" hover:bg-black transition-colors duration-200 border-r border-b border-[#2d2d2d] border-t-0 border-l-0     p-[35px] text-3xl font-thin text-start"
+      >
+        Expertise
+      </button>
+      {/* Bloc 2.2 */}
+      <div className="grid grid-cols-2">
+        {/* Bloc 2.2.1 */}
+        <button
+          onClick={() => {
+            scrollToSection("PortfolioGrid");
+            toggleMenu();
+          }}
+          className=" hover:bg-black transition-colors duration-200 border-r border-b border-[#2d2d2d] border-t-0 border-l-0     p-[35px] text-3xl font-thin text-start"
         >
-          <img
-            src={
-              darkMode
-                ? "/img/logo_dimagin_blanc_&_vert.png"
-                : "/img/logo_dimagin_noir_&_vert.png "
-            }
-            alt="Studio"
-            className="w-140 md:w-32 p-4 block md:hidden"
-          />
-          <div className="flex flex-col cursor-pointer space-y-4 pb-4">
-            {[
-              { id: "HeroSection", label: "Accueil" },
-              { id: "ExpertiseSection", label: "Expertise" },
-              { id: "PortfolioGrid", label: "Portfolio" },
-              { id: "Descriptif", label: "Descriptif" },
-              { id: "TeamSection", label: "Team" },
-              { id: "ValuesSection", label: "Nos Valeurs" },
-              { id: "contact", label: "Contactez-Nous" },
-            ].map(({ id, label }, index) => (
-              <button
-                key={id}
-                onClick={() => {
-                  scrollToSection(id);
-                  toggleMenu();
-                }}
-                className={`w-full cursor-pointer py-3 px-2 text-left text-lg transition-all duration-300 ${
-                  currentSection === id
-                    ? "text-gray-700  cursor-pointer font-extralight underline underline-offset-4"
-                    : `${
-                        darkMode
-                          ? "hover:text-gray-400 text-white"
-                          : "hover:text-gray-900 text-black"
-                      }`
-                } flex justify-between items-center ${
-                  index < 6 ? "border-b" : ""
-                } ${darkMode ? "border-gray-700" : "border-gray-400"}`}
-              >
-                <span>{label}</span>
-                <ArrowRight
-                  strokeWidth={0.5}
-                  size={24}
-                  className={`${
-                    currentSection === id
-                      ? "text-black"
-                      : `${darkMode ? "text-gray-400" : "text-gray-500"}`
-                  } transition-colors`}
-                />
-              </button>
-            ))}
-          </div>
+          Portfolio
+        </button>
+        {/* Bloc 2.2.2 */}
+        <a
+          href="mailto:start@dimagin.be"
+          className="hover:bg-black transition-colors duration-200 flex border-r border-b p-[35px] border-[#2d2d2d] border-t-0 border-l-0  font"
+        >
+          <span className="font-thin text-xl md:text-3xl">
+            start@dimagin.be
+          </span>
+          <ArrowUpRight strokeWidth={0.5} className=" " size={40} />
+        </a>
+      </div>
 
+      {/* Bloc 3 */}
+      {/* Bloc 3.1 */}
+      <button
+        onClick={() => {
+          scrollToSection("TeamSection");
+          toggleMenu();
+        }}
+        className="hover:bg-black transition-colors duration-200  border-r border-b border-[#2d2d2d] border-t-0 border-l-0  p-[35px] text-3xl font-thin text-start"
+      >
+        Notre équipe
+      </button>
+      {/* Bloc 3.2 */}
+      <div className="grid grid-cols-2">
+        {/* Bloc 3.2.1 */}
+        <button
+          onClick={() => {
+            scrollToSection("ValuesSection");
+            toggleMenu();
+          }}
+          className="hover:bg-black transition-colors duration-200  border-r border-b border-[#2d2d2d] border-t-0 border-l-0     p-[35px] text-3xl text-start font-thin "
+        >
+          Nos valeurs
+        </button>
+        {/* Bloc 3.2.2 */}
+        <button
+          onClick={() => {
+            scrollToSection("Footer");
+            toggleMenu();
+          }}
+          className="hover:bg-black transition-colors duration-200 border-r border-b border-[#2d2d2d] border-t-0 border-l-0  p-[35px] text-xl text-end font-thin"
+        >
+          Places des Arts 1, 4020 Liège
+          <br /> hello@dimagin.be <br /> +32 (0)456 38 41 24
+          <br />
+          Dimagin SRL BE 0805 685 463
+        </button>
+      </div>
+
+      {/* Bloc 4 */}
+      <div className=" border-r border-b border-[#2d2d2d] border-t-0 border-l-0 col-span-2 hover:bg-black flex items-center justify-between  ">
+        {/* contact */}
+        <div>
           <button
-            onClick={toggleMenu}
-            className={`absolute block  md:hidden top-4 right-4 transition-colors ${
-              darkMode
-                ? "text-gray-400 hover:text-gray-200"
-                : "text-gray-400 hover:text-gray-700"
-            }`}
-            aria-label="Fermer le menu"
+            onClick={() => {
+              scrollToSection("Footer");
+              toggleMenu();
+            }}
+            className=" col-span-2 flex  p-[35px] text-xl font-thin text-start"
           >
-            <X size={24} />
+            Contactez-nous
           </button>
-
-          <div
-            className={`border-t mt-4 pt-2 text-xs ${
-              darkMode
-                ? "border-gray-700 text-gray-400"
-                : "border-gray-400 text-gray-600"
-            }`}
-          >
-            © {new Date().getFullYear()} STUDIO DIMAGIN
-          </div>
         </div>
-
-        {/* Partie Droite */}
-        <div
-          className={`w-full sm:w-1/2 md:pt-20 sm:p-8 hidden md:flex flex-col justify-between items-center relative ${
-            darkMode ? "bg-black text-white" : "bg-white text-black"
-          }`}
-        >
-          <button
-            onClick={toggleMenu}
-            className={`absolute top-4 right-4 cursor-pointer transition-colors ${
-              darkMode
-                ? "text-gray-400 hover:text-gray-200"
-                : "text-gray-400 hover:text-gray-700"
-            }`}
-            aria-label="Fermer le menu"
-          >
-            <X size={24} />
-          </button>
-
-          <img
-            src={
-              darkMode
-                ? "/img/logo_dimagin_blanc_&_vert.png"
-                : "/img/logo_dimagin_noir_&_vert.png "
-            }
-            alt="Studio"
-            className="w-130 p-4"
+        {/* réseaux */}
+        <div className="p-4  text-xl font-thin  ">
+          <a href="" className=" p-4">
+            Instagram
+          </a>
+          <span
+            className={`inline-block w-2 md:w-6 h-[0.2px] mb-1 bg-white transition-all duration-300`}
           />
-
-          <div className="text-center space-y-1 font-thin text-sm sm:text-xl">
-            <p className="font-extralight">hello@dimagin.be</p>
-            <p>+32 (0)456 38 41 24</p>
-            <p>Places des Arts 1, 4020 Liège</p>
-            <p>Lu–Ve : 09h–17h</p>
-
-            <div className="flex justify-center gap-2 py-2 text-base sm:text-lg">
-              {socialLinks.map((social, index) => (
-                <a
-                  key={index}
-                  href={social.href}
-                  className={`transition-colors cursor-pointer  ${
-                    darkMode
-                      ? "text-gray-400 hover:text-white"
-                      : "text-gray-500 hover:text-black"
-                  }`}
-                >
-                  <social.icon />
-                </a>
-              ))}
-            </div>
-
-            <a
-              href="/privacy-policy"
-              className={`underline text-xs sm:text-sm cursor-pointer transition-colors ${
-                darkMode ? "hover:text-white" : "hover:text-black"
-              }`}
-            >
-              Politique de confidentialité
-            </a>
-          </div>
+          <a href="" className="p-4 ">
+            Linkedin
+          </a>{" "}
+          <span
+            className={`inline-block w-2 md:w-6 h-[0.5px] mb-1 bg-white transition-all duration-300`}
+          />
+          <a href="" className=" p-4">
+            Facebook
+          </a>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
